de5d9a59b8e9754a22d872d904b77dff43f4b096
Atif <coderatif@gmail.com>
2025-02-08 09:47:17 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const cron = require('node-cron')
+var cron = require('node-cron');
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!')
+})
+
+// run cron job every  30 seconds
+
+cron.schedule('* * * * *', () => {
+  console.log('running a task every minute')
+})
+cron.schedule('* * * * *', () => {
+    console.log('running a task every minute');
+  });
+
+app.listen('3000', (req, res) => {
+  console.log('Server is running on port 3000')
+})
